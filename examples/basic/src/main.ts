24a51f44278e32739fe72d2fa0687f42acb9d216
WhyNeet <arc.software.x@gmail.com>
2025-07-13 22:49:30 +0300

    feat(examples): update basic example

--- examples/basic/src/main.ts
+++ examples/basic/src/main.ts
@@ -1,14 +1,14 @@
-import { Table } from "engine";
+import { CellUpdateBuilder, Table } from "engine";
 import { TableRenderer } from "renderer";
 
 const canvas = document.getElementById("spreadsheet")! as HTMLCanvasElement;
 const cx = canvas.getContext("2d")!;
 
 const table = new Table();
+table.initCell([0, 0]);
+table.updateCell([0, 0], CellUpdateBuilder.content().data("Hello world! Cells engine is working fine."))
 const renderer = new TableRenderer(table, cx);
 
-renderer.resize();
+renderer.requestResize();
 
-console.time("Draw");
-renderer.draw();
-console.timeEnd("Draw");
+window.addEventListener("resize", () => renderer.requestResize());
