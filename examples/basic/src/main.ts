f8764db5d7b682a01e208ef2019374c3be6f5eff
WhyNeet <arc.software.x@gmail.com>
2025-07-14 22:33:26 +0300

    feat(example): update example

--- examples/basic/src/main.ts
+++ examples/basic/src/main.ts
@@ -1,5 +1,5 @@
 import { CellUpdateBuilder, Table } from "engine";
-import { TableRenderer } from "renderer";
+import { SpreadsheetInteractivity, TableRenderer, RendererProperties } from "renderer";
 
 const canvas = document.getElementById("spreadsheet")! as HTMLCanvasElement;
 const cx = canvas.getContext("2d")!;
@@ -7,7 +7,8 @@ const cx = canvas.getContext("2d")!;
 const table = new Table();
 table.initCell([0, 0]);
 table.updateCell([0, 0], CellUpdateBuilder.content().data("Hello world! Cells engine is working fine."))
-const renderer = new TableRenderer(table, cx);
+const renderer = new TableRenderer(table, cx, new RendererProperties([128, 28], 28, 48, 20, 2, [8, 6]));
+const interactivity = new SpreadsheetInteractivity(renderer.bundle());
 
 renderer.requestResize();
 
